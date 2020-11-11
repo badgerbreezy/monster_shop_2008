40c0b5d17b8c9eb05b2c15b441099ae909f90134
Brian Liu <brian.b.liu@gmail.com>
2020-11-10 19:33:50 -0700

    Add setup and add test subtotal with discount.

--- spec/models/item_orders_spec.rb
+++ spec/models/item_orders_spec.rb
@@ -14,7 +14,7 @@ describe ItemOrder, type: :model do
   end
 
   describe 'instance methods' do
-    it 'subtotal' do
+    before :each do
       @user = User.create!(name: "Batman",
                             address: "Some dark cave 11",
                             city: "Arkham",
@@ -25,12 +25,26 @@ describe ItemOrder, type: :model do
 
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
 
-      meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
-      tire = meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
-      order_1 = @user.orders.create!(name: 'Meg', address: '123 Stang Ave', city: 'Hershey', state: 'PA', zip: 17033)
-      item_order_1 = order_1.item_orders.create!(item: tire, price: tire.price, quantity: 2)
+      @meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+      @tire = @meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
+      @order_1 = @user.orders.create!(name: 'Meg', address: '123 Stang Ave', city: 'Hershey', state: 'PA', zip: 17033)
+      @item_order_1 = @order_1.item_orders.create!(item: @tire, price: @tire.price, quantity: 2)
+      @order_2 = @user.orders.create!(name: 'Meg', address: '123 Stang Ave', city: 'Hershey', state: 'PA', zip: 17033)
+      @item_order_2 = @order_2.item_orders.create!(item: @tire, price: @tire.price, quantity: 30)
+      @bulk_discount_1 = BulkDiscount.create!(
+        description: "5% on 20+",
+        discount_percent: 5,
+        minimum_quantity: 20,
+        merchant: @meg
+      )
+    end
 
-      expect(item_order_1.subtotal).to eq(200)
+    it 'subtotal with no discount' do
+      expect(@item_order_1.subtotal).to eq(200)
+    end
+
+    it 'subtotal with discount' do
+      expect(@item_order_2.subtotal).to eq(2850)
     end
   end
 end
